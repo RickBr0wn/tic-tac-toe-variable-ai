861662e47870fabf69cb5704636f070bf7d17eca
Rick Brown <ricky.brown.00@gmail.com>
2021-09-29 19:52:22 +0100

    Initial Commit

---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Rick Brown on 29/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
